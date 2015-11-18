642c8472ac81c3b689b7162eb59aa8437a19e318
JacksonMich <276099709@qq.com>
2015-11-18 10:51:39 +0800

    适配

---
+++ app/src/main/java/com/example/whx/hongmo/AdvertiseLaunch.java
@@ -0,0 +1,14 @@
+package com.example.whx.hongmo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AdvertiseLaunch extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertise_launch);
+    }
+
+}
